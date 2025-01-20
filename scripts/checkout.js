d301dc525f7074b6eb26e147b84dd0084319c787
fav-og1 <favourchingbawon@gmail.com>
2025-01-20 17:19:25 +0100

    comment header display

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,7 +5,7 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOption} from '../data/delivery.js';
 
  
-headerDisplay();
+//headerDisplay();
 
 const today = dayjs();
 
